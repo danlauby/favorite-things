9e9172d23659a0e30723361b232446152a9c13dc
danlauby <dmlauby@gmail.com>
2017-01-15 15:18:31 -0800

    Create ne array from original array and display in a un-ordered list

--- js/scripts.js
+++ js/scripts.js
@@ -43,14 +43,15 @@ $(function() {
     var favPerson = $('input[name="person"]').val();
     var userInputs = [];
     userInputs.push(name, favColor, favNum, favPerson);
-
+    var threeElements = [];
+    threeElements.push(userInputs[1], userInputs[0], userInputs[2]);
 
     var html;
     html += "<h2>Hi, <span id='personName'>";
     html += name;
     html += " </span>here is a list of your favorite things!</h2>";
     html += "<ul>";
-    $.each(userInputs, function(index, userInput) {
+    $.each(threeElements, function(index, userInput) {
       html += "<li>" + userInput + "</li>";
     });
     html += "</ul>";
